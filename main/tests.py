d88f5097ee3a3f41e9f921e30dabb761c6c1821c
aescobarr <agustiescobar@gmail.com>
2023-02-17 10:05:57 +0100

    Migrations reset

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class TestBasic(TestCase):
+    "Basic tests"
+
+    def test_basic(self):
+        a = 1
+        self.assertEqual(1, a)
+
+    def test_basic_2(self):
+        a = 1
+        assert a == 1
--- querystring_parser/bak_tests.py
+++ querystring_parser/bak_tests.py
@@ -9,8 +9,8 @@ Updated 2012-09-24 Bernard 'berni' Kobos
 '''
 
 import sys
-from parser import parse, MalformedQueryStringError
-from builder import build
+from querystring_parser.parser import parse, MalformedQueryStringError
+from querystring_parser.builder import build
 import unittest
 
 
@@ -111,7 +111,7 @@ class KnownValues(unittest.TestCase):
     def test_parse_known_values_with_unicode(self):
         """parse should give known result with known input (quoted)"""
         self.maxDiff = None
-        
+
         encoding = 'utf-8' if sys.version_info[0] == 2 else None
         for dic in self.knownValues + self.knownValuesWithUnicode:
             result = parse(build(dic, encoding=encoding), encoding=encoding)
