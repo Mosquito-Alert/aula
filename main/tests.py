7f2acb90bb8312eaa28900eb932a22184d7c7f0b
aescobarr <agustiescobar@gmail.com>
2023-02-20 16:13:58 +0100

    some more tests

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 from slugify import slugify
-from main.models import Campaign, get_current_active_campaign, EducationCenter, User, Profile, Quiz, Question
+from main.models import Campaign, get_current_active_campaign, EducationCenter, User, Profile, Quiz, Question, Answer, \
+    QuizRun, QuizRunAnswers
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient, APITestCase
 
 
 class CampaignTest(TestCase):
@@ -216,4 +218,155 @@ class EducationCenterTest(TestCase):
         e1 = EducationCenter.objects.get(name='educationcenter1')
         self.assertTrue( e1.name == str(e1), msg="Center string representation should be {0}, is {1}".format( e1.name, str(e1) ) )
 
+
+class PollTests(APITestCase):
+    def setUp(self) -> None:
+        c1 = Campaign(name='campaign1')
+        c1.active = True
+        c1.save()
+
+        e1 = EducationCenter(
+            name = "educationcenter1",
+            campaign = c1
+        )
+        e1.hashtag = e1.center_slug()
+        e1.save()
+
+        q2 = Quiz( name='quiz_poll_2', published=True, type=2, campaign=c1, seq= 2)
+        q2.save()
+
+        q2_1 = Question(quiz=q2, text='Poll question 1', question_order=1)
+        q2_1.save()
+
+        a2_1_1 = Answer( question=q2_1, label='A', text='A' )
+        a2_1_1.save()
+        a2_1_2 = Answer(question=q2_1, label='B', text='B')
+        a2_1_2.save()
+        a2_1_3 = Answer(question=q2_1, label='C', text='C')
+        a2_1_3.save()
+        a2_1_4 = Answer(question=q2_1, label='D', text='D')
+        a2_1_4.save()
+
+        q2_2 = Question(quiz=q2, text='Poll question 2', question_order=2)
+        q2_2.save()
+
+        a2_2_1 = Answer(question=q2_2, label='E', text='E')
+        a2_2_1.save()
+        a2_2_2 = Answer(question=q2_2, label='F', text='F')
+        a2_2_2.save()
+        a2_2_3 = Answer(question=q2_2, label='G', text='G')
+        a2_2_3.save()
+        a2_2_4 = Answer(question=q2_2, label='H', text='H')
+        a2_2_4.save()
+
+        teacher = User(username='teacher1')
+        teacher.set_password('123456')
+        teacher.save()
+        teacher.profile.teacher_password = '123456'
+        teacher.profile.is_teacher = True
+        teacher.profile.teacher_belongs_to = e1
+        teacher.save()
+
+        group1 = User(username='group1')
+        group1.set_password('123456')
+        group1.save()
+        group1.profile.is_group = True
+        group1.profile.group_password = '1234'
+        group1.profile.group_public_name = 'Group 1'
+        group1.profile.group_class = 'Class 1'
+        group1.profile.group_class_slug = slugify('Class 1')
+        group1.profile.group_teacher = teacher
+        group1.profile.campaign = c1
+        group1.profile.n_students_in_group = 3
+        group1.save()
+
+        group2 = User(username='group2')
+        group2.set_password('123456')
+        group2.save()
+        group2.profile.is_group = True
+        group2.profile.group_password = '1234'
+        group2.profile.group_public_name = 'Group 2'
+        group2.profile.group_class = 'Class 1'
+        group2.profile.group_class_slug = slugify('Class 1')
+        group2.profile.group_teacher = teacher
+        group2.profile.campaign = c1
+        group2.profile.n_students_in_group = 3
+        group2.save()
+
+    def tearDown(self) -> None:
+        group1 = User.objects.get(username='group1')
+        group2 = User.objects.get(username='group2')
+        teacher = User.objects.get(username='teacher1')
+
+        group1.delete()
+        group2.delete()
+        teacher.delete()
+
+        c1 = Campaign.objects.get(name='campaign1')
+        e1 = EducationCenter.objects.get(name = "educationcenter1")
+        e1.delete()
+        c1.delete()
+
+        q2 = Quiz.objects.get(name='quiz_poll_2')
+        q2.delete()
+
+    def test_poll_results(self):
+        e1 = EducationCenter.objects.get(name="educationcenter1")
+        group1 = User.objects.get(username='group1')
+        self.client.force_authenticate(user=group1)
+        q2 = Quiz.objects.get(name='quiz_poll_2')
+        data = {
+            'quiz_id': q2.id,
+            'taken_by': group1.id,
+            'run_number': 1
+        }
+        response = self.client.post('/api/startrun/', data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue( QuizRun.objects.filter(taken_by=group1).exists(), "Quizrun should have been created for quiz {0}, group {1}".format(q2, group1))
+
+        group2 = User.objects.get(username='group2')
+        self.client.force_authenticate(user=group1)
+        data = {
+            'quiz_id': q2.id,
+            'taken_by': group2.id,
+            'run_number': 1
+        }
+        response = self.client.post('/api/startrun/', data=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(QuizRun.objects.filter(taken_by=group1).exists(), "Quizrun should have been created for quiz {0}, group {1}".format(q2, group2))
+
+        group1_quizrun = QuizRun.objects.get(taken_by=group1)
+        group1_quizrunanswers = QuizRunAnswers.objects.filter(quizrun=group1_quizrun).order_by('question__question_order')
+        for quizrunanswer in group1_quizrunanswers:
+            question = quizrunanswer.question
+            answer = question.sorted_answers_set[0]
+            data = {
+                'id': quizrunanswer.id,
+                'answer_id': answer.id
+            }
+            response = self.client.post('/api/writeanswer/', data=data)
+            self.assertEqual(response.status_code, 200)
+
+        group2_quizrun = QuizRun.objects.get(taken_by=group2)
+        group2_quizrunanswers = QuizRunAnswers.objects.filter(quizrun=group2_quizrun).order_by('question__question_order')
+        for quizrunanswer in group2_quizrunanswers:
+            question = quizrunanswer.question
+            answer = question.sorted_answers_set[0]
+            data = {
+                'id': quizrunanswer.id,
+                'answer_id': answer.id
+            }
+            response = self.client.post('/api/writeanswer/', data=data)
+            self.assertEqual(response.status_code, 200)
+
+        a = Answer.objects.get(label='A', text='A')
+        self.assertTrue(a.how_many_times_answered == 2, "Answer A should have been answered 2 times, has been {0} times".format(a.how_many_times_answered))
+        self.assertTrue(a.how_many_times_answered_by_group(group1) == 1, "Answer A should have been answered 1 time by group 1, has been {0} times".format(a.how_many_times_answered_by_group(group1)))
+        self.assertTrue(a.how_many_times_answered_by_group(group2) == 1, "Answer A should have been answered 1 time by group 2, has been {0} times".format(a.how_many_times_answered_by_group(group2)))
+        self.assertTrue(a.how_many_times_answered_by_center(e1.id) == 2, "Answer A should have been answered 2 times by center 1, has been {0} times".format(a.how_many_times_answered_by_center(e1.id)))
+        e = Answer.objects.get(label='E', text='E')
+        self.assertTrue(e.how_many_times_answered == 2, "Answer E should have been answered 2 times, has been {0} times".format(e.how_many_times_answered))
+        self.assertTrue(e.how_many_times_answered_by_group(group1) == 1, "Answer E should have been answered 1 time by group 1, has been {0} times".format(e.how_many_times_answered_by_group(group1)))
+        self.assertTrue(e.how_many_times_answered_by_group(group2) == 1, "Answer E should have been answered 1 time by group 2, has been {0} times".format(e.how_many_times_answered_by_group(group2)))
+        self.assertTrue(e.how_many_times_answered_by_center(e1.id) == 2, "Answer E should have been answered 2 times by center 1, has been {0} times".format(e.how_many_times_answered_by_center(e1.id)))
 
