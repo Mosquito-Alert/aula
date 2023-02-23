ea6725e43726c178f3203b5c8c30a07f68c4de8e
aescobarr <agustiescobar@gmail.com>
2023-02-23 14:20:41 +0100

    Further tweaks to admin forms

--- main/admin.py
+++ main/admin.py
@@ -5,14 +5,29 @@ from django.contrib import admin
 from .models import *
 
 
-class QuizRunAdmin(admin.ModelAdmin):
-    list_display = ('id', 'taken_by', 'quiz', 'date','date_finished','run_number')
-    search_fields = ('taken_by__username', 'quiz__name', 'date','date_finished')
+class QuestionLinkInline(admin.TabularInline):
+    model = Question
+    fields = ('question_order', 'text', 'doc_link', 'question_picture')
+    show_change_link = True
+
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline,]
 
 
 class QuizAdmin(admin.ModelAdmin):
     list_display = ('id', 'author', 'name', 'html_header', 'published', 'type_text', 'campaign')
     search_fields = ('author__username', 'name', 'campaign__name')
+    inlines = [ QuestionLinkInline, ]
+
+
+class QuizRunAdmin(admin.ModelAdmin):
+    list_display = ('id', 'taken_by', 'quiz', 'date','date_finished','run_number')
+    search_fields = ('taken_by__username', 'quiz__name', 'date','date_finished')
 
 
 class ProfileAdmin(admin.ModelAdmin):
@@ -20,13 +35,14 @@ class ProfileAdmin(admin.ModelAdmin):
 
 
 class EducationCenterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'campaign',)
     search_fields = ('name', 'campaign__name', )
 
 
 admin.site.register(EducationCenter, EducationCenterAdmin)
 admin.site.register(Quiz, QuizAdmin)
 admin.site.register(QuizRun, QuizRunAdmin)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Campaign)
 admin.site.register(Profile, ProfileAdmin)
 #admin.site.register(GroupAnswer)
