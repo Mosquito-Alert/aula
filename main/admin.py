94390014d3ef092def237e6cd9e9bc4d9df1bb81
aescobarr <agustiescobar@gmail.com>
2023-02-23 12:44:20 +0100

    More tweaks to admin

--- main/admin.py
+++ main/admin.py
@@ -4,19 +4,24 @@ from django.contrib import admin
 
 from .models import *
 
+
 class QuizRunAdmin(admin.ModelAdmin):
     list_display = ('id', 'taken_by', 'quiz', 'date','date_finished','run_number')
     search_fields = ('taken_by__username', 'quiz__name', 'date','date_finished')
 
+
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'name', 'html_header', 'published', 'type_text')
-    search_fields = ('author__username', 'name', 'type_text')
+    list_display = ('id', 'author', 'name', 'html_header', 'published', 'type_text', 'campaign')
+    search_fields = ('author__username', 'name', 'campaign__name')
+
 
 class ProfileAdmin(admin.ModelAdmin):
     search_fields = ( 'user__username', )
 
+
 class EducationCenterAdmin(admin.ModelAdmin):
     search_fields = ('name', 'campaign__name', )
+
 
 admin.site.register(EducationCenter, EducationCenterAdmin)
 admin.site.register(Quiz, QuizAdmin)
